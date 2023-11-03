a7dda91918a89d2170d39df1c9a2192b1a5b68c3
JustHuman228 <justhuman228.github@gmail.com>
2023-11-03 13:14:20 +0300

    Document `module-info.java`
    Add a basic documentation to `module-info.java`
    of the `jlatenter.test` module. Nothing special.

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -1,3 +1,22 @@
+/**
+ * Stores all the unit tests of the JLatenter library.
+ *
+ * <p>
+ * This module is considered internal, so the user can't and shouldn't access anything in this module, including
+ * Javadocs. All the Javadocs here provided only and only because why not, and not included in the global
+ * documentation.
+ * </p>
+ *
+ * <p>
+ * <b>
+ * Note: This module also uses <code>org.jetbrains:annotations:24.0.1</code>'s annotations to enable a better static
+ * analysis, but it isn't required, so if you don't provide the dependency yourself, then all annotations will be
+ * ignored, and if you provide it - your IDE should enable better static analysis automatically.
+ * </b>
+ * </p>
+ *
+ * @since 0.1-build.2
+ */
 module jlatenter.test {
 
 	requires java.base;
